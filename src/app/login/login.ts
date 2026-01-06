20e8c4e5ce872af9253f2e1c61bd4c97beb54a5a
melika-nasehi <130899479+melika-nasehi@users.noreply.github.com>
2026-01-06 16:48:41 +0330

    added login page with JWT token

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../services/auth.services';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrl: './login.css'
 })
 export class Login {
+  credentials = {
+    username: '',
+    password: ''
+  };
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onLogin() {
+    this.authService.login(this.credentials).subscribe({
+      next: () => {
+        this.router.navigate(['/home']);
+      },
+      error: (err) => {
+        alert('Login failed! Check console.');
+        console.error(err);
+      }
+    });
+  }
 }
